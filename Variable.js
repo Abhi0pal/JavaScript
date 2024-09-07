2aa9cbaa19f63215f2cba7addbdfd534941753ea
Abhi0pal <abhishekpal941163@gmail.com>
2024-09-07 15:25:03 +0530

    var

---
+++ Variable.js
@@ -0,0 +1,11 @@
+const accountId=1445553
+let accountEmail="abhishek11@gmail.com"
+var accountPassword="1#4%56&)66"
+accountCity="kanpur"
+let accountState;
+//we have two method to print 
+// one method
+    // console.log(accountId);
+    // console.log(accountEmail);
+//second Method
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
