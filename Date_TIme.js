fe28ce829df76878e4cc4bfeefe8c877eb9de9ce
Abhi0pal <abhishekpal941163@gmail.com>
2024-09-08 14:43:51 +0530

    Date_TIme

---
+++ Date_TIme.js
@@ -0,0 +1,8 @@
+let newDate=new Date;
+// console.log(newDate.toString());
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.getMonth());
+// console.log(newDate.getFullYear());
+console.log(newDate.getDay());
