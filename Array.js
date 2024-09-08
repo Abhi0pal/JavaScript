2fd95caf9fdb2ba5a7dcbf363acb7259009c1cf6
Abhi0pal <abhishekpal941163@gmail.com>
2024-09-09 02:32:31 +0530

    array

---
+++ Array.js
@@ -0,0 +1,19 @@
+// const arr=[1,2,3,4,5,"Happy","True"]
+// arr.push(100)
+// arr.unshift(0)
+//unshist is a method where we push value in the starting of the array
+// console.log(arr.includes(0));
+// console.log(arr.indexOf(1));
+//concept of slice or splice
+
+//spread opreator
+
+const arr1=[1,2,3,4,5];
+const arr2=[6,7,8,9,0];
+const arr3=[...arr1,...arr2];
+ console.log(arr3);
+
+ 
+
+
+// console.log(0)
